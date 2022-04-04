cc161e069ab75faa219fb87e6b853a3c1bd1a132
Michael BAUDIN <michael.baudin@gmail.com>
2022-04-04 15:01:36 +0200

    Creates new graphics

--- smolyak/smolyak_polychaos/smolyak_chaospy_uniform.py
+++ smolyak/smolyak_polychaos/smolyak_chaospy_uniform.py
@@ -13,9 +13,12 @@ Output
 import chaospy
 from matplotlib import pyplot
 
-def plot_smolyak_nodes(rule, level, verbose=False, factor = 5.0e2):
+def plot_smolyak_nodes(vector_distribution, rule, level, verbose=False, factor = 5.0e2):
+    if len(vector_distribution) != len(rule):
+        raise ValueError("Dimension of distribution is %d, but there are %d quadrature rules" % (
+            len(vector_distribution), len(rule)))
     sparse_nodes, sparse_weights = chaospy.generate_quadrature(
-        level, joint, rule=rule, sparse=True
+        level, vector_distribution, rule=rule, sparse=True
     )
     if verbose:
         print("rule = ", rule)
@@ -38,9 +41,12 @@ def plot_smolyak_nodes(rule, level, verbose=False, factor = 5.0e2):
     pyplot.ylim(-1.0 - epsilon, 1.0 + epsilon)
     return None
 
-def print_smolyak_nodes(rule, level, verbose=False, format_nodes="%-20s", format_markdown=True):
+def print_smolyak_nodes(vector_distribution, rule, level, verbose=False, format_nodes="%-20s", format_markdown=True):
+    if len(vector_distribution) != len(rule):
+        raise ValueError("Dimension of distribution is %d, but there are %d quadrature rules" % (
+            len(vector_distribution), len(rule)))
     nodes, weights = chaospy.generate_quadrature(
-        level, joint, rule=rule, sparse=True
+        level, vector_distribution, rule=rule, sparse=True
     )
     dimension = nodes.shape[0]
     size = nodes.shape[1]
@@ -54,45 +60,116 @@ def print_smolyak_nodes(rule, level, verbose=False, format_nodes="%-20s", format
             print("x[%d] : [%s], w[%d] = %.4f" % (i, final_x_print, i, weights[i]))
     return None
 
+def plot_smolyak_grid(number_of_rows, number_of_columns, vector_distribution, rule, 
+                      factor):
+    level = 0
+    for i in range(number_of_rows):
+        for j in range(number_of_columns):
+            index = level + 1
+            pyplot.subplot(number_of_rows, number_of_columns, index)
+            plot_smolyak_nodes(vector_distribution, rule, level, verbose=False, factor=factor)
+            pyplot.title("level = %d" % (level))
+            level += 1
+    return None
+
 
 x1 = chaospy.Uniform(-1.0, 1.0)
 x2 = chaospy.Uniform(-1.0, 1.0)
-joint = chaospy.J(x1, x2)
-print("joint distribution")
-print(joint)
+vector_distribution = chaospy.J(x1, x2)
+print("vector_distribution distribution")
+print(vector_distribution)
 #
 print("+ Smolyak sparse-grid")
 
 rule=["gaussian", "gaussian"]
 level = 3
-plot_smolyak_nodes(rule, level, verbose=False)
+plot_smolyak_nodes(vector_distribution, rule, level, verbose=False)
 pyplot.show()
 
 #
-print("+ Plot different levels")
+print("+ Plot different levels for Smolyak-Legendre")
 factor = 100.0
-level = 0
-for i in range(2):
-    for j in range(3):
-        index = level + 1
-        pyplot.subplot(2, 3, index)
-        plot_smolyak_nodes(rule, level, verbose=False, factor=factor)
-        pyplot.title("level = %d" % (level))
-        level += 1
+number_of_rows = 2
+number_of_columns = 3
+rule=["gaussian", "gaussian"]
+print("rule = ", rule)
+plot_smolyak_grid(number_of_rows, number_of_columns, vector_distribution, rule, 
+                      factor)
 pyplot.subplots_adjust(wspace=0.3, hspace = 0.7)
 pyplot.suptitle("Smolyak-Legendre")
 pyplot.show()
 
 #
-print("+ Print nodes and weights")
+print("+ Plot different levels for Smolyak-Clenshaw-Curtis")
+factor = 100.0
+number_of_rows = 2
+number_of_columns = 3
+rule=["clenshaw_curtis", "clenshaw_curtis"]
+print("rule = ", rule)
+plot_smolyak_grid(number_of_rows, number_of_columns, vector_distribution, rule, 
+                      factor)
+pyplot.subplots_adjust(wspace=0.3, hspace = 0.7)
+pyplot.suptitle("Smolyak-Clenshaw-Curtis")
+pyplot.show()
+
+#
+print("+ Plot different levels for Smolyak-Fejér")
+factor = 100.0
+number_of_rows = 2
+number_of_columns = 3
+rule=["fejer", "fejer"]
+print("rule = ", rule)
+plot_smolyak_grid(number_of_rows, number_of_columns, vector_distribution, rule, 
+                      factor)
+pyplot.subplots_adjust(wspace=0.3, hspace = 0.7)
+pyplot.suptitle("Smolyak-Fejér")
+pyplot.show()
+
+#
+print("+ Print nodes and weights for Smolyak-Legendre (dimension 2)")
 rule=["gaussian", "gaussian"]
 print("rule = ", rule)
 for level in range(1, 4):
     print("Level = ", level)
-    print_smolyak_nodes(rule, level, verbose=False, format_nodes="%-20s")
+    print_smolyak_nodes(vector_distribution, rule, level, verbose=False, format_nodes="%-20s")
 
+print("+ Print nodes and weights for Smolyak-Clenshaw-Curtis (dimension 2)")
 rule=["clenshaw_curtis", "clenshaw_curtis"]
 print("rule = ", rule)
 for level in range(1, 4):
     print("Level = ", level)
-    print_smolyak_nodes(rule, level, verbose=False, format_nodes="%-20s")
+    print_smolyak_nodes(vector_distribution, rule, level, verbose=False, format_nodes="%-20s")
+
+print("+ Print nodes and weights for Smolyak-Fejér (dimension 2)")
+rule=["fejer", "fejer"]
+print("rule = ", rule)
+for level in range(1, 4):
+    print("Level = ", level)
+    print_smolyak_nodes(vector_distribution, rule, level, verbose=False, format_nodes="%-20s")
+
+print("+ Print nodes and weights for Smolyak-Clenshaw-Curtis (dimension 1)")
+rule=["clenshaw_curtis"]
+x1 = chaospy.Uniform(-1.0, 1.0)
+vector_distribution = chaospy.J(x1)
+print("rule = ", rule)
+for level in range(1, 6):
+    print("Level = ", level)
+    print_smolyak_nodes(vector_distribution, rule, level, verbose=False, format_nodes="%-20s")
+
+print("+ Print nodes and weights for Smolyak with uniform distribution (dimension 1)")
+level = 5
+x1 = chaospy.Uniform(-5.0, 5.0)
+vector_distribution = chaospy.J(x1)
+for rule in (["clenshaw_curtis"], ["fejer"]):
+    print("rule = ", rule)
+    print("Level = ", level)
+    print_smolyak_nodes(vector_distribution, rule, level, verbose=False, format_nodes="%-20s")
+
+print("+ Print nodes and weights for Smolyak with normal distribution (dimension 1)")
+level = 5
+x1 = chaospy.Normal(1.0, 4.0)
+vector_distribution = chaospy.J(x1)
+for rule in (["clenshaw_curtis"], ["fejer"]):
+    print("rule = ", rule)
+    print("Level = ", level)
+    print_smolyak_nodes(vector_distribution, rule, level, verbose=False, format_nodes="%-20s")
