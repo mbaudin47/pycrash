07d771f6261ce78d194c68c1c8e7bcbd16867bf8
Michael BAUDIN <michael.baudin@gmail.com>
2025-05-07 18:18:32 +0200

    test_PCE_LARS.py: Corrige les commentaires

--- PCE_LARS/test_PCE_LARS.py
+++ PCE_LARS/test_PCE_LARS.py
@@ -1,8 +1,31 @@
-"""Implement the LARS algorithm in Python"""
+"""Implements the selection method of a polynomial chaos expansion algorithm in Python.
+
+This implements 2 algorithms:
+- Algorithm B.1 page 628 of (Lüthen, et al., 2021),
+- Algorithm B.1 with with CV using Corrected Leave-One-Out or K-Fold.
+
+TODO-List
+---------
+- Extend the code to multiple output dimensions.
+- Early stopping of the algorithm using the K-Fold or Corrected LOO score.
+- Implement LARS using algorithm B.2.
+
+Reference
+---------
+- Lüthen, N., Marelli, S., & Sudret, B. (2021). 
+  Sparse polynomial chaos expansions: Literature survey and benchmark. 
+  SIAM/ASA Journal on Uncertainty Quantification, 9(2), 593-649.
+
+"""
 
 # %%
 import openturns as ot
 from openturns.usecases import ishigami_function
+import openturns.viewer as otv
+import numpy as np
+
+# %%
+ot.RandomGenerator.SetSeed(0)
 
 # %%
 im = ishigami_function.IshigamiModel()
@@ -84,9 +107,22 @@ leastSquaresMethod = ot.LeastSquaresMethod.Build(
     leastSquaresMethodName, designProxy, list_of_active_functions
 )
 residuals = output_sample.asPoint()
+# Select your best fitting algorithm
+# fitting = ot.CorrectedLeaveOneOut()
+kParameter = 10
+fitting = ot.KFold(kParameter)
+# Compute initial fitting score
+fitting_score = fitting.run(
+    standard_input,
+    output_sample,
+    ot.Point(sample_size, 1) / sample_size,
+    functions,
+    list_of_active_functions,
+)
+print(f"  Fitting score = {fitting_score:.4e}")
+fitting_score_list = [fitting_score]
 # Update residuals
 residuals -= ot.Point(sample_size, output_sample.computeMean()[0])
-# TODO: subtract the sample mean
 # TODO: Repeat this for output each marginal
 for i in range(maximum_basis_dimension - 1):
     # Find candidate with maximum absolute correlation with the residual
@@ -119,15 +155,20 @@ for i in range(maximum_basis_dimension - 1):
     designMatrix = leastSquaresMethod.computeWeightedDesign()
     residuals = output_sample.asPoint() - designMatrix * coefficients
     # Compute corrected leave-out score
-    cloo = ot.CorrectedLeaveOneOut()
-    score = cloo.run(
+    fitting_score = fitting.run(
         standard_input,
         output_sample,
         ot.Point(sample_size, 1) / sample_size,
         functions,
         list_of_active_functions,
     )
-    print(f"  CLOO score = {score:.4e}")
+    """
+    # TODO: add this. See https://github.com/openturns/openturns/issues/2948
+    fitting_score = fitting.run(leastSquaresMethod, output_sample)
+    """
+    print(f"  Fitting score = {fitting_score:.4e}")
+    fitting_score_list.append(fitting_score)
+    # Compute KFold
 
 coefficientSample = ot.Sample.BuildFromPoint(coefficients)
 
@@ -159,5 +200,28 @@ result
 meta_model = result.getMetaModel()
 validation = ot.MetaModelValidation(output_test, meta_model(input_test))
 print(f"Q2 = {validation.computeR2Score()[0]:.15f}")
+
+# %%
+threshold = ot.ResourceMap.GetAsScalar("SparseMethod-ErrorThreshold")
+error_factor = ot.ResourceMap.GetAsScalar("SparseMethod-MaximumErrorFactor")
+min_index = np.argmin(fitting_score_list)
+fitting_score_min = np.min(fitting_score_list)
+graph = ot.Graph(
+    f"{fitting.getClassName()}", "Iteration", f"{fitting.getClassName()} score", True
+)
+cloud = ot.Cloud(range(maximum_basis_dimension), fitting_score_list)
+graph.add(cloud)
+graph.setLogScale(ot.GraphImplementation.LOGY)
+# Plot min corrected score
+cloud = ot.Cloud([min_index], [fitting_score_min])
+cloud.setPointStyle("circle")
+cloud.setLegend("Min")
+graph.add(cloud)
+# Plot error factor
+curve = ot.Curve([0, maximum_basis_dimension], [error_factor * fitting_score_min] * 2)
+curve.setLineWidth(2.0)
+curve.setLegend("Treshold")
+graph.add(curve)
+otv.View(graph)
 
 # %%
