6e6631a9e1a2c4a86bac23b3b71cc3eaec9199d2
Michael BAUDIN <michael.baudin@gmail.com>
2026-05-09 18:37:47 +0200

    hyperbolic_enumeration/ Ajoute les scripts du cours consacrés à la règle hyperbolique

---
+++ hyperbolic_enumeration/hyperbolic_enumeration_rule.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Experiment with the hyperbolic enumeration rule.
+"""
+import openturns as ot
+
+
+def compute_strata_from_degree(
+    enumerateFunction, total_degree=10, maximum_strata_index=100
+):
+    """
+    Compute the minimum index of the strata corresponding to a given total degree.
+
+    This is the smallest index of a strata which contains a multiindex
+    having the required total degree.
+    Usually, this function is used with a large maximumu strata index, so that
+    a large number of stratas are explored until the required total degree
+    is reached.
+
+    The returned index is so that this strata contains a multi-index having
+    a total degree equal to the required total_degree, but interaction multi-indices
+    may be lower that this total_degree.
+
+    This is a Python replicate of OpenTURNS's getStrataCumulatedCardinal(),
+    but this function works properly, while OpenTURNS's has a bug.
+
+    Parameters
+    ----------
+    enumerateFunction : ot.EnumerateFunction()
+        The enumerate function.
+    total_degree : int
+        The total degree to reach.
+    maximum_strata_index : int
+        The maximum number of strata to try.
+
+    Returns
+    -------
+    degree_strata_index : int
+        The index of the strata containing a multiindex having given total
+        degree.
+        It is lower of equal to maximum_strata_index.
+        It may be equal to maximum_strata_index if no multiindex
+        has reached the given total degree.
+
+    """
+    is_degree_reached = False
+    degree_strata_index = 0
+    print("Hyperbolic rule, $q=%.2f$." % (quasi_norm_parameter))
+    for strata_index in range(1 + maximum_strata_index):
+        if is_degree_reached:
+            break
+        print("strata_index = ", strata_index)
+        strata_cardinal = enumerateFunction.getStrataCardinal(strata_index)
+        cumulated_cardinal = enumerateFunction.getStrataCumulatedCardinal(strata_index)
+        number_of_indices_in_strata = cumulated_cardinal - strata_cardinal
+        for i in range(number_of_indices_in_strata, cumulated_cardinal):
+            multiindex = enumerateFunction(i)
+            multiindex_degree = sum(multiindex)
+            if multiindex_degree >= total_degree:
+                print(
+                    "Maximum degree reached : ",
+                    multiindex_degree,
+                    ", multiindex = ",
+                    multiindex,
+                    ", strata_index = ",
+                    strata_index,
+                )
+                is_degree_reached = True
+                degree_strata_index = strata_index
+                break
+    return degree_strata_index
+
+
+def print_up_to_maximum_strata_index(enumerateFunction, degree_strata_index):
+    for strata_index in range(1 + degree_strata_index):
+        print("+ strata index = ", strata_index)
+        strata_cardinal = enumerateFunction.getStrataCardinal(strata_index)
+        cumulated_cardinal = enumerateFunction.getStrataCumulatedCardinal(strata_index)
+        number_of_indices_in_strata = cumulated_cardinal - strata_cardinal
+        for i in range(number_of_indices_in_strata, cumulated_cardinal):
+            multiindex = enumerateFunction(i)
+            print("    ", multiindex)
+    return None
+
+
+def getMaximumDegreeStrataIndex(enumerateFunction, maximumDegree):
+    # From https://github.com/openturns/openturns/blob/f38378d3ea9bff8414786cdb30ff55a917f6eac5/lib/src/Base/Func/HyperbolicAnisotropicEnumerateFunction.cxx#L200
+    # find indice
+    index = 0
+    degree = 0
+    # First, a geometrical search to find an upper bound
+    # Old : while (degree <= maximumDegree):
+    while degree < maximumDegree:
+        multiindex = enumerateFunction(index)
+        degree = sum(multiindex)
+        index += 1
+    strataIndex = 0
+    while enumerateFunction.getStrataCumulatedCardinal(strataIndex) < index:
+        strataIndex += 1
+    # Old : return strataIndex - 1
+    return strataIndex
+
+
+dimension = 2
+quasi_norm_parameter = 0.5
+maximum_strata_index = 100
+total_degree = 10
+enumerateFunction = ot.HyperbolicAnisotropicEnumerateFunction(
+    dimension, quasi_norm_parameter
+)
+degree_strata_index = compute_strata_from_degree(
+    enumerateFunction, total_degree, maximum_strata_index
+)
+print("degree_strata_index = ", degree_strata_index)
+
+degree_strata_index_bis = enumerateFunction.getMaximumDegreeStrataIndex(total_degree)
+print("degree_strata_index_bis = ", degree_strata_index_bis)
+
+degree_strata_index_ter = getMaximumDegreeStrataIndex(enumerateFunction, total_degree)
+print("degree_strata_index_ter = ", degree_strata_index_ter)
+
+
+print("Hyperbolic rule, q=%.2f." % (quasi_norm_parameter))
+print_up_to_maximum_strata_index(enumerateFunction, degree_strata_index_ter)
---
+++ hyperbolic_enumeration/test_HyperbolicRule.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Get the number of polynomials lower than a given degree.
+"""
+import openturns as ot
+import numpy as np
+
+
+def print_up_to_maximum_strata_index(enumerateFunction, degree_strata_index):
+    for strata_index in range(1 + degree_strata_index):
+        print("+ strata index = ", strata_index)
+        strata_cardinal = enumerateFunction.getStrataCardinal(strata_index)
+        cumulated_cardinal = enumerateFunction.getStrataCumulatedCardinal(strata_index)
+        number_of_indices_in_strata = cumulated_cardinal - strata_cardinal
+        for i in range(number_of_indices_in_strata, cumulated_cardinal):
+            multiindex = enumerateFunction(i)
+            print("    ", multiindex, " sum=", sum(multiindex))
+    return None
+
+
+dimension = 2
+quasi_norm_parameter = 0.5
+polynomial_degree = 10
+
+enumerateFunction = ot.HyperbolicAnisotropicEnumerateFunction(
+    dimension, quasi_norm_parameter
+)
+strata_index = enumerateFunction.getMaximumDegreeStrataIndex(polynomial_degree)
+cardinal = enumerateFunction.getStrataCardinal(strata_index)
+cumulated_cardinal = enumerateFunction.getStrataCumulatedCardinal(strata_index)
+
+print("dimension=", dimension)
+print("quasi_norm_parameter=", quasi_norm_parameter)
+print("polynomial_degree=", polynomial_degree)
+print("strata_index=", strata_index)
+print("cardinal=", cardinal)
+print("cumulated_cardinal=", cumulated_cardinal)
+
+
+print_up_to_maximum_strata_index(enumerateFunction, strata_index)
+
+
+class QuasiNorm(ot.OpenTURNSPythonFunction):
+    def __init__(self, dimension, quasi_norm_parameter):
+        super(QuasiNorm, self).__init__(dimension, 1)
+        self.quasi_norm_parameter = quasi_norm_parameter
+
+    def _exec(self, x):
+        x = ot.Point(x)
+        dimension = x.getDimension()
+        norm = 0.0
+        for i in range(dimension):
+            norm += x[i] ** self.quasi_norm_parameter
+        norm = np.exp(np.log(norm) / self.quasi_norm_parameter)
+        return [norm]
+
+
+quasi_norm = ot.Function(QuasiNorm(dimension, quasi_norm_parameter))
+
+
+if False:
+    polynomial_degree = 3
+    strata_index = enumerateFunction.getMaximumDegreeStrataIndex(polynomial_degree)
+
+    strata_cardinal = enumerateFunction.getStrataCardinal(strata_index)
+    cumulated_cardinal = enumerateFunction.getStrataCumulatedCardinal(strata_index)
+    number_of_indices_in_strata = cumulated_cardinal - strata_cardinal
+    for i in range(number_of_indices_in_strata, cumulated_cardinal):
+        multiindex = enumerateFunction(i)
+        qnorm = quasi_norm(multiindex)
+        total_degree = sum(multiindex)
+        print(multiindex, "Q-Norm=", qnorm, "Total degree=", total_degree)
